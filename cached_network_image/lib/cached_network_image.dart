0d4fd35f88cfaecd640ae57b791c10614ea4961a
Eren Gun <erengun4@gmail.com>
2026-06-10 12:02:17 +0300

    feat: add HttpInterceptor and CacheInterceptor abstract classes with data types

--- cached_network_image/lib/cached_network_image.dart
+++ cached_network_image/lib/cached_network_image.dart
@@ -18,6 +18,8 @@ export 'package:cached_network_image_platform_interface_ce/cached_network_image_
         UnsupportedImageFormatException;
 
 export 'src/cache/default_cache_manager_factory.dart';
+export 'src/cache/interceptors/cache_interceptor.dart';
+export 'src/cache/interceptors/http_interceptor.dart';
 export 'src/cached_image_widget.dart';
 export 'src/image_provider/cached_network_image_provider.dart';
 export 'src/image_provider/multi_image_stream_completer.dart';
---
+++ cached_network_image/lib/src/cache/interceptors/http_interceptor.dart
@@ -0,0 +1,87 @@
+import 'package:http/http.dart' as http;
+
+/// Mutable request data passed through the HTTP interceptor chain.
+class HttpRequestData {
+  HttpRequestData({required this.url, required this.headers});
+
+  String url;
+  Map<String, String> headers;
+}
+
+/// Immutable response data passed through the HTTP interceptor chain.
+class HttpResponseData {
+  const HttpResponseData({required this.response, required this.originalUrl});
+
+  final http.StreamedResponse response;
+  final String originalUrl;
+}
+
+/// Handler for [HttpInterceptor.onRequest].
+///
+/// Call exactly one of [next], [resolve], or [reject].
+class HttpRequestHandler {
+  HttpRequestHandler._(this._onNext, this._onResolve, this._onReject);
+
+  final void Function(HttpRequestData) _onNext;
+  final void Function(HttpResponseData) _onResolve;
+  final void Function(Object, StackTrace?) _onReject;
+
+  void next(HttpRequestData request) => _onNext(request);
+  void resolve(HttpResponseData response) => _onResolve(response);
+  void reject(Object error, [StackTrace? stackTrace]) =>
+      _onReject(error, stackTrace);
+}
+
+/// Handler for [HttpInterceptor.onResponse].
+class HttpResponseHandler {
+  HttpResponseHandler._(this._onNext, this._onResolve, this._onReject);
+
+  final void Function(HttpResponseData) _onNext;
+  final void Function(HttpResponseData) _onResolve;
+  final void Function(Object, StackTrace?) _onReject;
+
+  void next(HttpResponseData response) => _onNext(response);
+  void resolve(HttpResponseData response) => _onResolve(response);
+  void reject(Object error, [StackTrace? stackTrace]) =>
+      _onReject(error, stackTrace);
+}
+
+/// Handler for [HttpInterceptor.onError].
+class HttpErrorHandler {
+  HttpErrorHandler._(this._onNext, this._onResolve, this._onReject);
+
+  final void Function(Object, StackTrace) _onNext;
+  final void Function(HttpResponseData) _onResolve;
+  final void Function(Object, StackTrace?) _onReject;
+
+  void next(Object error, StackTrace stackTrace) => _onNext(error, stackTrace);
+  void resolve(HttpResponseData response) => _onResolve(response);
+  void reject(Object error, [StackTrace? stackTrace]) =>
+      _onReject(error, stackTrace);
+}
+
+/// Abstract interceptor for HTTP requests and responses.
+///
+/// Override any hook you need; all default to passing through unchanged.
+///
+/// Each hook receives a typed data object and a handler. You MUST call
+/// exactly one of [handler.next], [handler.resolve], or [handler.reject]:
+/// - [next]: pass (possibly mutated) data to the next interceptor
+/// - [resolve]: short-circuit the chain with a synthetic response
+/// - [reject]: short-circuit the chain with an error
+abstract class HttpInterceptor {
+  const HttpInterceptor();
+
+  void onRequest(HttpRequestData request, HttpRequestHandler handler) =>
+      handler.next(request);
+
+  void onResponse(HttpResponseData response, HttpResponseHandler handler) =>
+      handler.next(response);
+
+  void onError(
+    Object error,
+    StackTrace stackTrace,
+    HttpErrorHandler handler,
+  ) =>
+      handler.next(error, stackTrace);
+}
